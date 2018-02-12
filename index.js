ab659527898734a635b037e0fb172cc2d707baa1
Oghene Michael <zonalmeet@gmail.com>
2018-02-12 20:14:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var cart = [];
 
-function getCart() {
- return cart;
+function getCart(itemName) {
+ return cart.push(itemName);
 }
 
 function setCart(c) {
@@ -10,7 +10,13 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  const price = Math.floor(Math.random() * 100) + 1;
+
+  cart.push({ [item]: price });
+
+  console.log(`${item} has been added to your cart.`);
+
+  return cart;
 }
 
 function viewCart() {
@@ -26,5 +32,8 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (!cardNumber) {
+    console.log(`Sorry, we don't have a credit card on file for you.`);
+    return false
+  }
 }
